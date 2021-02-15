180eaee443e05d78361194289dc29424135b3c8f
Musixen <altenburgern@bzz.ch>
2021-02-15 13:49:41 +0100

    Finished the NavBar

---
+++ Blog/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    var nav = $('.nav');
+    var toggleCollapse = $('.toggle-collapse');
+
+
+    /**Click event on toggle menu*/
+    toggleCollapse.click(function(){
+        nav.toggleClass('collapse');
+    });
+
+
+});
