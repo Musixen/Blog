b2c44283a79ce85b7c539078a5fad44b1ccc188f
Musixen <altenburgern@bzz.ch>
2021-02-16 14:18:42 +0100

    Finished hard codded website

--- Blog/js/main.js
+++ Blog/js/main.js
@@ -9,5 +9,22 @@ $(document).ready(function(){
         nav.toggleClass('collapse');
     })
 
+    //click to scroll top
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    //Discover Button Scroll function
+    $('.btn-discover').click(function(){
+        $('html, body').animate({
+            scrollTop: $('.site-content').offset().top},
+            'slow');
+    });
+
+    //AOS instance
+    AOS.init();
+
 
 });
